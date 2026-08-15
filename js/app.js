918f920a9bf78f50280ac94eec517137668140e2
Sofia-DC-M <sofia.dcm.ar@gmail.com>
2026-08-14 21:43:26 -0300

    Etapa 1: Primeros calculos de poliza en JS

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//#region Etapa 1
+const costoBase = 1.16
+const metrosCuadrados = Number(prompt("Ingrese cantidad de metros cuadrados"))
+const fm = 2.5
+
+var total = costoBase * metrosCuadrados * fm
+
+alert("monto total de la poliza: " + total) */
+//#endregion
